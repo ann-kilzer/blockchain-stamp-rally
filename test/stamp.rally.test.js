bcdb22e67db13ba2250ae7d0a29cce647d618bc3
Ann Kilzer <akilzer@gmail.com>
2019-01-01 17:57:32 +0900

    start testing

--- test/stamp.rally.test.js
+++ test/stamp.rally.test.js
@@ -4,13 +4,25 @@ const should = require('chai').should();
 
 contract('StampRally', function(accounts) {
 
+    let prefix = "http://www.example.com/stamp_rally/";
+    let numStamps = 12;
+    
+    let rally;
+    
     beforeEach(async function () {
-
+	rally = await StampRally.new(numStamps, prefix);
     });
 
-    describe("constructor", function() {
+    describe("constructor", async function() {
+	it("should make a new stamp rally", async function() {
+	    assert.equal((await rally.prefix()), prefix, "Prefix didn't match");
+	    assert.equal((await rally.numStamps()).toNumber(), numStamps, "NumStamps didn't match");
+	});
+    });
 
-    }
-    
+    describe("collect stamp", async function() {
+	it("should create a rally if one doesn't exist yet", async function() {
 
-};
+	});
+    });
+});
