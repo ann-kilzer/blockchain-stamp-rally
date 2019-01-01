3da2d9e1f909bf8f236ce94bf63a29db3d9df392
Ann Kilzer <akilzer@gmail.com>
2019-01-01 15:44:47 +0900

    upgrade to sol 0.5.0

--- contracts/StampRally.sol
+++ contracts/StampRally.sol
@@ -1,44 +1,44 @@
-pragma solidity 0.4.24;
+pragma solidity 0.5.0;
 
 
 contract StampRally {
   string public prefix; // url prefix for the stamp images
   uint8 public numStamps;
   struct Stamp {
-    bytes32 private hashedPassphrase;
+    bytes32 hashedPassphrase;
 
   }
 
   struct Rally {
-    Stamp[] public stamps;
-    bool public valid;
+    Stamp[] stamps;
+    bool valid;
   }
   
   Rally[] public rallies;
 
   mapping (address => Rally) public playerToRally;
 
-  constructor(uint8 _numStamps, string _prefix) {
+  constructor(uint8 _numStamps, string memory _prefix) public {
     prefix = _prefix;
     numStamps = _numStamps;
   }
 
 
   function initializeRally(address _player) internal {
-    Rally storage rally = playserToRally[_player];
+    Rally storage rally = playerToRally[_player];
     rally.valid = true;
     rally.stamps = new Stamp[](numStamps);
   }
   
-  function collectStamp(uint8 position, string passphrase) public {
-    Rally memory rally = playserToRally[msg.sender];
-    if (!rally.valid) {
-      initializeRally(msg.sender);
-    }
+  function collectStamp(uint8 position, string memory passphrase) public {
+    //Rally memory rally = playerToRally[msg.sender];
+    //if (!rally.valid) {
+    //  initializeRally(msg.sender);
+    //}
     // todo
   }
 
-  function getStamp(uint8 position) public view returns (string) {
+  function getStamp(uint8 position) public view returns (string memory) {
 
 
   }
