699f7a1bb19f22b7f34eebcadc3400a545f37168
Ann Kilzer <akilzer@gmail.com>
2019-07-25 23:32:33 +0900

    port javascript

--- webui/src/main.js
+++ webui/src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue';
 import App from './App.vue';
+import Vuetify from 'vuetify';
+import colors from 'vuetify/es5/util/colors';
 
 Vue.config.productionTip = false;
 
+Vue.use(Vuetify, {
+  theme: {
+    primary: colors.blue.darken1,
+    secondary: colors.orange.lighten5,
+    accent: colors.red.accent3
+  }
+});
+
+/* eslint-disable no-undef */
+let StampRallyJson = require('./StampRally.json');
+/* esline-enable no-undef */
+
 new Vue({
-  render: h => h(App),
-}).$mount('#app');
+  el: '#app',
+  data () {
+    return {
+      json: StampRallyJson
+    };
+  },
+  render: h => h(App)
+});
